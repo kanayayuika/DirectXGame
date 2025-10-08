cedb156927a95394ac10f86100553da657e2e058
kanayayuika <kanaya.yuika@gmail.com>
2025-10-08 16:27:34 +0900

    CG2_00_04

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <Windows.h>
 #include <cstdint>// int32_tを使う用
+#include <format>
+#include <string>
+#include <filesystem>// ファイルやディレクトリに関する操作を行うライブラリ
 
 #pragma region ウィンドウプロシージャ(CG2_00_03_4P)
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -14,6 +17,42 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 	// 標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+#pragma endregion
+
+#pragma region Log関数
+// Log関数(stringのみ受け付ける関数)
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+// stringからwstringの変換関数
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+// wstringからstringの変換関数
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
 }
 #pragma endregion
 
@@ -81,8 +120,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			// ゲームの処理
 		}
 	}
+	
+	// Log出力
+	std::wstring wstringValue = L"a";
+	Log(ConvertString(std::format(L"WSTRING:{}\n", wstringValue)));
 
-	// 出力会陰同への文字出力
+	// 出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
 
 	return 0;
